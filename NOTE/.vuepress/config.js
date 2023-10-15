6ab5ef631151d636a98a7e89d867bc9d982d43e0
ALLENYGY <allenygy@outlook.com>
2023-10-15 20:18:32 +0800

    new

--- NOTE/.vuepress/config.js
+++ NOTE/.vuepress/config.js
@@ -11,9 +11,10 @@ module.exports = {
       { text: "编程语言", link: "" },
       { text: "数学", link: "Math" },
       { text: "数据库", link: "" },
+      { text: "计算机组成原理", link: "CO" },
     ],
     group: {
-      "Math": [
+      Math: [
         {
           text: "线性代数",
           sublink: [
@@ -25,6 +26,12 @@ module.exports = {
         },
         { text: "离散数学", link: "" },
         { text: "概率统计", link: "" },
+      ],
+      CO: [
+        { text: "Computer", link: "CO/Lecture/Lec1" },
+        { text: "Bits: Data Representation and Manipulation", link: "CO/Lecture/Lec2" },
+        { text: "Boolean Algebra: From Bits to Logic", link: "CO/Lecture/Lec3" },
+        { text: "From Transistors to Gates", link: "CO/Lecture/Lec4" },
       ],
     },
     editurl:
